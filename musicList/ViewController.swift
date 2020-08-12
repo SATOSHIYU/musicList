f9281c5ac93ce19cae01ada48e031982b14b679a
YU <virgo850823@gmail.com>
2020-08-12 15:12:35 +0800

    Initial Commit

---
+++ musicList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  musicList
+//
+//  Created by HSIEH CHIH YU on 2020/8/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
